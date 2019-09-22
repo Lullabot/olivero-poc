183d01918f5ac7cee11b3e72439e920574531464
Mike Herchel <mike@lullabot.com>
2019-09-22 14:17:51 -0400

    Work on desktop nav interactions.

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,7 +1,44 @@
 "use strict";
 
 (function () {
-  var hey = "hey!";
-  console.log(hey);
+  // document.querySelector('.nav-primary__button').addEventListener('click', e => {
+  //   e.target.classList.toggle('js-active');
+  // });
+  var fixables = document.querySelectorAll('.fixable');
+
+  function monitorNavPosition() {
+    var primaryNav = document.querySelector('.site-header');
+    var options = {
+      rootMargin: '72px',
+      threshold: [0.999, 1]
+    };
+    var observer = new IntersectionObserver(toggleDesktopNavVisibility, options);
+    observer.observe(primaryNav);
+  }
+
+  function toggleDesktopNavVisibility(entries) {
+    if (!isDesktopNav()) return;
+    entries.forEach(function (entry) {
+      // Every pixel is visible at 1.
+      console.log(entry.intersectionRatio);
+
+      if (entry.intersectionRatio < 1) {
+        fixables.forEach(function (el) {
+          return el.classList.add('js-fixed');
+        });
+      } else {
+        fixables.forEach(function (el) {
+          return el.classList.remove('js-fixed');
+        });
+      }
+    });
+  }
+
+  function isDesktopNav() {
+    var navButton = document.querySelector('.header-nav__button');
+    return window.getComputedStyle(navButton).getPropertyValue('display') === 'none';
+  }
+
+  monitorNavPosition();
 })();
 //# sourceMappingURL=scripts.js.map
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,45 @@
 (function() {
-  const hey = "hey!"
-  console.log(hey);
+  // document.querySelector('.nav-primary__button').addEventListener('click', e => {
+  //   e.target.classList.toggle('js-active');
+  // });
+
+  const fixables = document.querySelectorAll('.fixable')
+
+  function monitorNavPosition() {
+    const primaryNav = document.querySelector('.site-header');
+    const options = {
+      rootMargin: '72px',
+      threshold: [0.999, 1]
+    }
+
+    const observer = new IntersectionObserver(toggleDesktopNavVisibility, options);
+    observer.observe(primaryNav);
+  }
+
+  function toggleDesktopNavVisibility(entries) {
+    if (!isDesktopNav()) return;
+
+    entries.forEach(entry => {
+      // Every pixel is visible at 1.
+      console.log(entry.intersectionRatio);
+
+      if (entry.intersectionRatio < 1) {
+        fixables.forEach(el => el.classList.add('js-fixed'))
+      }
+      else {
+        fixables.forEach(el => el.classList.remove('js-fixed'))
+      }
+    })
+
+
+  }
+
+
+  function isDesktopNav() {
+    const navButton = document.querySelector('.header-nav__button');
+    return window.getComputedStyle(navButton).getPropertyValue('display') === 'none';
+  }
+
+  monitorNavPosition();
+
 })();
