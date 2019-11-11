756a1547e483510656f0c3212d4d8800716fd520
haeberle <f.haeberle@tojio.com>
2019-11-11 12:26:04 +0100

    Revert "fix focus trap in navigation"
    This reverts commit 29d234dadc7c956560dc34bfcca5eeb1d89ca7c2.

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -62,12 +62,12 @@
   mobileNavWrapper.addEventListener('keydown', function(e) {
     if (e.key === 'Tab' || e.keyCode === 9) {
       if ( e.shiftKey ) /* shift + tab */ {
-        if (document.activeElement === firstFocusableEl && mobileNavButton.offsetParent !== null) {
+        if (document.activeElement === firstFocusableEl) {
           mobileNavButton.focus();
           e.preventDefault();
         }
       } else /* tab */ {
-        if (document.activeElement === lastFocusableEl && mobileNavButton.offsetParent !== null) {
+        if (document.activeElement === lastFocusableEl) {
           mobileNavButton.focus();
           e.preventDefault();
         }
