cd21a6665372382e0fdf67a0361d0448b2b323e3
Juanluis Lozano <vulcanr@gmail.com>
2019-11-16 18:10:02 +0100

    Remove aria targets where not needed, add function to init js classes and other classes toggle

--- src/js/second-level-navigation.js
+++ src/js/second-level-navigation.js
@@ -1,5 +1,6 @@
-(function() {
-  const secondLevelNavMenus = document.querySelectorAll('.primary-nav--level-1 [aria-haspopup="true"]');
+(function () {
+  const secondLevelNavMenus = document.querySelectorAll('.primary-nav--level-1 [aria-expanded="false"]');
+  const subMenu = document.querySelector('.primary-nav--level-2');
 
   // Insert a button after the <a> element that will control the visibility
   // of the second-level navigation.
@@ -9,25 +10,36 @@
 
     button.classList.add('primary-nav__button-expand');
     button.textContent = 'Expand sub-navigation';
+    button.setAttribute('aria-expanded', 'false');
     button.addEventListener('click', expandSubNav);
     el.insertBefore(button, subMenu);
   });
 
   function expandSubNav(e) {
     const button = e.target;
-    const subMenu = button.parentNode.querySelector('.primary-nav--level-2');
 
     if (!isButtonPressed(button)) {
       button.setAttribute('aria-pressed', 'true');
-      subMenu.setAttribute('aria-expanded', 'true');
+      button.setAttribute('aria-expanded', 'true');
+      subMenu.classList.add('primary-nav--level-2--visible');
     }
     else {
       button.setAttribute('aria-pressed', 'false');
-      subMenu.setAttribute('aria-expanded', 'false');
+      button.setAttribute('aria-expanded', 'false');
+      subMenu.classList.remove('primary-nav--level-2--visible');
     }
+
   }
 
   function isButtonPressed(el) {
     return el.getAttribute('aria-pressed') === 'true';
   }
+
+  function init() {
+    //Removes via JS hardcoded class to be shown by default
+    subMenu.classList.remove('primary-nav--level-2--visible');
+  }
+
+  init();
+
 })();
