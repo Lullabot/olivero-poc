83c02b7040534dede09c4146e441a1e9e585bb49
Juanluis Lozano <vulcanr@gmail.com>
2019-11-16 18:09:35 +0100

    Navigation events fixes

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,15 +1,28 @@
-(function() {
+(function () {
 
   const isDesktopNav = drupalSettings.olivero.isDesktopNav;
 
-  const mobileNavButton = document.querySelector('.mobile-nav-button');
+  const mobileNavButton = document.createElement('button');
+  const mobileNavButtonWrapper = document.querySelector('.mobile-buttons');
   const mobileNavWrapperId = 'header-nav';
   const mobileNavWrapper = document.getElementById(mobileNavWrapperId);
+  const navGridHolder = document.querySelector('.grid-full-no-js');
   const body = document.querySelector('body');
   const overlay = document.querySelector('.overlay');
+  let mobileNavButtonIcon;
 
   function init() {
-    mobileNavButton.setAttribute('aria-controls', mobileNavWrapperId);
+    // Adds the markup inside the button, sets the aria elements, and removes if JS present the class 'grid-full-no-js'
+
+    mobileNavButton.innerHTML = '<div class="mobile-nav-button__label">Menu</div><div class="mobile-nav-button__icon"></div>';
+    mobileNavButtonWrapper.appendChild(mobileNavButton);
+    mobileNavButton.classList.add('mobile-nav-button');
+    mobileNavButton.setAttribute('aria-controls', 'header-nav');
+    mobileNavButton.setAttribute('aria-expanded', 'false');
+    mobileNavWrapper.classList.add('js-header-nav');
+    navGridHolder.classList.remove('grid-full-no-js');
+    document.querySelector('.primary-nav--level-2').classList.add('js-primary-nav--level-2');
+    mobileNavButtonIcon = document.querySelector('.mobile-nav-button__icon');
   }
 
   function isMobileNavOpen() {
@@ -18,16 +31,20 @@
 
   function toggleMobileNav(state) {
     const value = state ? 'true' : 'false';
-    mobileNavWrapper.setAttribute('aria-expanded', value);
-    mobileNavButton.setAttribute('aria-pressed', value);
 
-    // Overlay
+    mobileNavWrapper.classList.toggle('header-nav--visible');
+    mobileNavButtonIcon.classList.toggle('mobile-nav-button__icon--active');
+    body.classList.toggle('js-overlay-active');
+    body.classList.toggle('js-fixed');
+
     if (state) {
-      body.classList.add('js-overlay-active', 'js-fixed');
+      mobileNavButton.setAttribute('aria-pressed', 'true');
+      mobileNavButton.setAttribute('aria-expanded', 'true');
+    } else {
+      mobileNavButton.setAttribute('aria-pressed', 'false');
+      mobileNavButton.setAttribute('aria-expanded', 'false');
     }
-    else {
-      body.classList.remove('js-overlay-active', 'js-fixed');
-    }
+
   }
 
   // Initialize everything.
