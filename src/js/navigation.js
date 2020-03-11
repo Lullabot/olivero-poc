0163588bc1bf5fefe015e82c782ca777dd96c213
Brian Perry <contact.brianperry@gmail.com>
2020-03-11 09:42:30 -0500

    Revert "13476281: added intersectionObserver polyfill"
    This reverts commit 5e388b0a5bc09633fd0561cd67952b962e28e35c.

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -89,6 +89,7 @@
   });
 
   // If site branding and primary nav collide, force mobile nav
+  // TODO - IE / Polyfill and then possibly remove this if.
   if ("IntersectionObserver" in window) {
     if (!isDefaultMobileNav) {
       const siteBrandingObserver = new IntersectionObserver(
